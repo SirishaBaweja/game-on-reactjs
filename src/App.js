6a08228fdae89d18bea23d7a051a0f664ec6e5dc
sirisha <19uec175@gmail.com>
2021-04-09 03:51:35 +0530

    created squares

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React from "react"
+import Board from "./components/Board"
 
-const App=() => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
-export default App; 
+const App = () => {
+  return (
+    <div>
+      <h1>TICTACTOE</h1>
+      <Board />
+    </div>
+  );
+};
+
+export default App;
+
+
