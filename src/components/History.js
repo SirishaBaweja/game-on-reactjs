a09de0841f2ec45803d6e85957db4860c3b94670
sirisha <19uec175@gmail.com>
2021-04-11 12:22:19 +0530

    changed

--- src/components/History.js
+++ src/components/History.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-const History = ({history,moveTo,currentmove}) => {
-    return (
-        <ul>
-            {history.map((_,move)=>{return(
-           <li key={move}><button Style={{
-               fontWeight: move===currentmove? "bold":"normal",
-           }}type="button" onClick={()=>{
-               moveTo(move)
-           }}>{move===0?'Go to Game start':`Go to move #${move}`}</button></li>) })}
-        </ul>
-    )
-}
+const History = ({ history, moveTo, currentMove }) => {
+  return (
+    <ul>
+      {history.map((_, move) => {
+        return (
+          <li key={move}>
+            <button
+              style={{
+                fontWeight: move === currentMove ? 'bold' : 'normal',
+              }}
+              type="button"
+              onClick={() => {
+                moveTo(move);
+              }}
+            >
+              {move === 0 ? 'Go to game start' : `Go to move #${move}`}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
-export default History
+export default History;
+
